9e97b39eb98660f3facfd969657db1b93c1b2fda
Erik <erik.fernandes87@gmail.com>
2025-03-29 18:47:39 -0300

    workflow dispatch

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "public_subnets" {
   description = "Public subnets used by VPC Link"
   type        = list(string)
+  default     = []
 }
 
 variable "vpc_link_sg_id" {
   description = "Security Group ID used by VPC Link"
   type        = string
+  default     = ""
 }
 
 variable "alb_dns_name" {
   description = "DNS name of the ALB used for integration"
   type        = string
+  default     = ""
 }
